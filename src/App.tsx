4eec048b2c92142d40e49f5a04a4d472d7855e79
Sean Patten <seanpatten84@gmail.com>
2025-09-21 12:23:07 +0200

    Added component - SeanHero

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 
 import { Navbar } from './components/Navbar.js';
+import SeanHero from './components/SeanHero.js';
 function App() {
   return (
+    <div>
     <Navbar/>
+    <SeanHero/>
+    </div>
   );
 }
 
---
+++ src/components/SeanHero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SeanHero = () => {
+  return (
+    <div className=""></div>
+  )
+}
+
+export default SeanHero
