f30ecc442b2c50aaebc72d95dadc3531c07c0c46
Sean Patten <seanpatten84@gmail.com>
2025-09-21 13:23:53 +0200

    Added component - Skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import { Navbar } from './components/Navbar.js';
 import SeanHero from './components/SeanHero.js';
+import Skills from './components/Skills.js';
 function App() {
   return (
     <div>
     <Navbar/>
     <SeanHero/>
+    <Skills/>
     </div>
   );
 }
