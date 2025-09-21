dc022d94a27b256228a9e1c29d91e315f3aebb7a
Sean Patten <seanpatten84@gmail.com>
2025-09-21 11:43:04 +0200

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import { Navbar } from './components/Navbar.js';
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind is working! 🚀
-      </h1>
-    </div>
+    <Navbar/>
   );
 }
 
