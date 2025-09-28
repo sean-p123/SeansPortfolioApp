c020cebf17ce5a7a4f2b5499d6349fe2dd3dcbfe
Sean Patten <seanpatten84@gmail.com>
2025-09-28 17:44:26 +0200

    css update - Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export function Navbar() {
   
   return (
       <div>
-        <div className="bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
+        <div className="border border-gray-600 bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
           <h1 className="text-3xl font-bold primary-color ml-4">S. Patten</h1>
           <ul className="hidden md:flex">
             <li className="p-5"><a href="#about">About</a></li>
@@ -19,7 +19,7 @@ export function Navbar() {
           <div onClick={handleNav} className="block md:hidden">
             {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/> }
           </div>
-          <div className={nav ? 'fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500]' : 'fixed left-[-100%]'}>
+          <div className={nav ? 'z-10 fixed h-full left-0 top-0 w-[60%] bg-[#202121] ease-in-out duration-500]' : 'fixed left-[-100%]'}>
             <h1 className="text-3xl primary-color m-4">S. Patten</h1>
             <ul className="p-8 text-2xl">
             <li className="p-2"><a href="#home">Home</a></li>
