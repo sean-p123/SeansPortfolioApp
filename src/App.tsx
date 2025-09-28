5ddff5b5fdc90389461865893c03d50aa776838b
Sean Patten <seanpatten84@gmail.com>
2025-09-28 17:02:25 +0200

    Added component - Work

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import About from './components/About.js';
 import { Navbar } from './components/Navbar.js';
 import SeanHero from './components/SeanHero.js';
 import Skills from './components/Skills.js';
+import Work from './components/Work.js';
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
     <SeanHero/>
     <Skills/>
     <About/>
+    <Work/>
     </div>
   );
 }
