dbbf67603d5809cc04e42993e5830571ba7c005d
Sean Patten <seanpatten84@gmail.com>
2025-12-02 17:52:11 +0100

    added nodejs field and rearranging others

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,6 +6,7 @@ import reactImg from '../assets/img/react.png';
 import angularImg from '../assets/img/angular.png';
 import tailwindImg from '../assets/img/tailwind.png';
 import mysqlImg from '../assets/img/mysql2.png';
+import nodejs from '../assets/img/nodejs.png';
 const Skills = () => {
   return (
     <div className='bg-black border border-gray-600 text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-6
@@ -16,33 +17,39 @@ const Skills = () => {
         </h2>
 
         <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={htmlImg} alt=''/>
-          <p className='mt-2'>HTML</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={cssImg} alt='' width={100} height={100}/>
-          <p className='mt-2'>CSS</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={jsImg} alt='' width={100} height={100}/>
+          <img src={jsImg} alt='' className="w-24 h-24 object-contain"/>
           <p className='mt-2'>Javascript</p>
         </div>
         <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={tailwindImg} alt='' width={100} height={100}/>
-          <p className='mt-2'>Tailwind</p>
+          <img src={nodejs} alt='' className="w-24 h-24 object-contain" />
+          <p className='mt-2'>NodeJs</p>
         </div>
         <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={mysqlImg} alt='' width={100} height={100}/>
-          <p className='mt-2'>MySql</p>
-        </div>
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={angularImg} alt='' width={100} height={100}/>
+          <img src={angularImg} alt='' className="w-24 h-24 object-contain"/>
           <p className='mt-2'>Angular</p>
         </div>
         <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
-          <img src={reactImg} alt='' width={100} height={100}/>
+          <img src={reactImg} alt='' className="w-24 h-24 object-contain"/>
           <p className='mt-2'>React</p>
         </div>
+         <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+          <img src={mysqlImg} alt='' className="w-24 h-24 object-contain"/>
+          <p className='mt-2'>MySql</p>
+        </div>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+          <img src={htmlImg} alt='' className="w-24 h-24 object-contain"/>
+          <p className='mt-2'>HTML</p>
+        </div>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+          <img src={cssImg} alt='' className="w-24 h-24 object-contain"/>
+          <p className='mt-2'>CSS</p>
+        </div>
+        
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[80px] md:w-[100px]'>
+          <img src={tailwindImg} alt='' className="w-24 h-24 object-contain"/>
+          <p className='mt-2'>Tailwind</p>
+        </div>
+        
       </div>
   )
 }
